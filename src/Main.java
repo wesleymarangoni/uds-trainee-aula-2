08a3bb63494cf4681a7578d6df23e77728a9f96e
John Smith <wesleymarangoni@hotmail.com>
2018-06-16 11:29:03 -0300

    Atualizando todo projeto

--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,7 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<String> arrayMeusDados = new ArrayList();
-        arrayMeusDados.add(0, "Wesley");
-        arrayMeusDados.add(1, "87050-220");
-        arrayMeusDados.add(2, "Av Seilá");
-        arrayMeusDados.add(3, "122");
-        arrayMeusDados.add(4, "Frente");
-        arrayMeusDados.add(5, "Jd Luz");
-        arrayMeusDados.add(6, "Maringá");
-        arrayMeusDados.add(6, "Paraná");
-        arrayMeusDados.add(7, "44 9 9878-1380");
-
-        arrayMeusDados.set(0, "Wesley Marangoni Rodrigues");
-        arrayMeusDados.remove(7);
-        imprimirArray(arrayMeusDados);
-    }
-    public static void imprimirArray(ArrayList arrayMeusDados){
-        for (int i=0;i<arrayMeusDados.size();i++){
-            System.out.println(arrayMeusDados.get(i));
-        }
+        Pessoa pessoa = new Pessoa();
+        System.out.println(pessoa.obterInformacao());
     }
 }
