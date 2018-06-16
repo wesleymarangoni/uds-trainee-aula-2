6c194651ef36fcfde580a11e53667ffe8bcdae87
John Smith <wesleymarangoni@hotmail.com>
2018-06-16 08:53:09 -0300

    primeiro commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args){
+        ArrayList<String> arrayMeusDados = new ArrayList();
+        arrayMeusDados.add(0,"Wesley");
+        arrayMeusDados.add(1,"87050-220");
+        arrayMeusDados.add(2,"Av Seilá");
+        arrayMeusDados.add(3,"122");
+        arrayMeusDados.add(4,"Frente");
+        arrayMeusDados.add(5,"Jd Luz");
+        arrayMeusDados.add(6,"Maringá");
+        arrayMeusDados.add(6,"Paraná");
+        arrayMeusDados.add(7,"44 9 9878-1380");
+
+        arrayMeusDados.set(0,"Wesley Marangoni Rodrigues");
+        arrayMeusDados.set(7,"");
+        //System.out.println(meuArrayBonitao.get(0));
+    }
+}
